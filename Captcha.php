a5277e868f9543b8cb270a6174cd1cf026c60559
Phanlop P <phanlop@Phanlops-MacBook-Pro.local>
2014-03-11 15:55:02 +0700

    testGetResultOperation

--- Captcha.php
+++ Captcha.php
@@ -1,10 +1,10 @@
 <?php
 class Captcha {
 	
-	function __construct($mode, $leftOperand, $operator, $rightOperand){
+	function __construct($pattern, $leftOperand, $operator, $rightOperand){
 		$this->listNumberWithString = array(1=>"One",2=>"Two",3=>"Three",4=>"Four",5=>"Five",6=>"Six",7=>"Seven",8=>"Eight",9=>"Nine");
 		$this->operator = $operator;
-		$this->mode = 1;
+		$this->pattern = $pattern;
 		$this->rightOperand = $rightOperand;
 		$this->leftOperand = $leftOperand;
 	}
@@ -13,8 +13,15 @@ class Captcha {
 		return "One + 1";
 	}
 	
+	private function getNumberText($number){
+		return $this->listNumberWithString[$number];
+	}
+	private function isLeftTextNumberPattern(){
+		return $this->pattern == 1;
+	}
+	
 	function getLeftOperand() {
-		return $this->listNumberWithString[$this->leftOperand];
+		return $this->isLeftTextNumberPattern() ? $this->getNumberText($this->leftOperand) : $this->leftOperand;
 	}
 	
 	function getOperation() {
@@ -26,15 +33,25 @@ class Captcha {
 			case 3:
 				return "-";
 		}
-
 	}
+	
 	
 	function getRightOperand() {
-		return $this->rightOperand;
+		return $this->isLeftTextNumberPattern() ? $this->rightOperand : $this->getNumberText($this->rightOperand);
 	}
-	
+
 	function getResult() {
-		return 2;
+
+		switch ($this->operator){
+			case 1:
+				return $this->leftOperand + $this->rightOperand;
+			case 2:
+				return $this->leftOperand * $this->rightOperand;
+			case 3:
+				return $this->leftOperand - $this->rightOperand;
+		}
+		
+	 
 	}
 }
 ?>
--- CaptchaTest.php
+++ CaptchaTest.php
@@ -43,6 +43,43 @@ class CaptchaTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals("Eight", (new Captcha(1,8,1,1))->getLeftOperand());
 		$this->assertEquals("Nine", (new Captcha(1,9,1,1))->getLeftOperand());
 	}
+	
+	function testRightOperandModeTwoShouldReturnNumber(){
+		$this->assertEquals("One", (new Captcha(2,1,1,1))->getRightOperand());
+		$this->assertEquals("Two", (new Captcha(2,1,1,2))->getRightOperand());
+		$this->assertEquals("Three", (new Captcha(2,1,1,3))->getRightOperand());
+		$this->assertEquals("Four", (new Captcha(2,1,1,4))->getRightOperand());
+		$this->assertEquals("Five", (new Captcha(2,1,1,5))->getRightOperand());
+		$this->assertEquals("Six", (new Captcha(2,1,1,6))->getRightOperand());
+		$this->assertEquals("Seven", (new Captcha(2,1,1,7))->getRightOperand());
+		$this->assertEquals("Eight", (new Captcha(2,1,1,8))->getRightOperand());
+		$this->assertEquals("Nine", (new Captcha(2,1,1,9))->getRightOperand());
+	}
+	
+	function testLeftOperandModeTwoShouldReturnStringOfNumber(){
+		$this->assertEquals("1", (new Captcha(2,1,1,1))->getLeftOperand());
+		$this->assertEquals("2", (new Captcha(2,2,1,1))->getLeftOperand());
+		$this->assertEquals("3", (new Captcha(2,3,1,1))->getLeftOperand());
+		$this->assertEquals("4", (new Captcha(2,4,1,1))->getLeftOperand());
+		$this->assertEquals("5", (new Captcha(2,5,1,1))->getLeftOperand());
+		$this->assertEquals("6", (new Captcha(2,6,1,1))->getLeftOperand());
+		$this->assertEquals("7", (new Captcha(2,7,1,1))->getLeftOperand());
+		$this->assertEquals("8", (new Captcha(2,8,1,1))->getLeftOperand());
+		$this->assertEquals("9", (new Captcha(2,9,1,1))->getLeftOperand());
+	}
+	function testGetResultPlusOperation() {
+		$this->assertEquals(2, (new Captcha(1,1,1,1))->getResult());
+		$this->assertEquals(3, (new Captcha(1,2,1,1))->getResult());
+		$this->assertEquals(123, (new Captcha(1,23,1,100))->getResult());
+	}
+	function testGetResultMultiplyOperation() {
+		$this->assertEquals(1, (new Captcha(1,1,2,1))->getResult());
+	    $this->assertEquals(35, (new Captcha(1,7,2,5))->getResult());
+	}
+	function testGetResultMinusOperation() {
+		$this->assertEquals(0, (new Captcha(1,1,3,1))->getResult());
+		$this->assertEquals(5, (new Captcha(1,7,3,2))->getResult());
+	}
 }
 
 
